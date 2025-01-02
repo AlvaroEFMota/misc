90c69a40b57c4a6199361a89ccd7090f1a088915
Álvaro E. F. Mota <alvaroefmota@gmail.com>
2025-01-02 09:36:28 -0300

    add an interleaved merge in linked list

--- problem-solving/src/bin/linked_list.rs
+++ problem-solving/src/bin/linked_list.rs
@@ -59,17 +59,91 @@ impl LinkedList {
             }
         }
     }
+
+    fn print(&self) {
+        if let Some(root_node) = &self.root {
+            let mut tmp = Rc::clone(root_node);
+            print!("{}", tmp.borrow().value);
+            while let Some(node) = &tmp.clone().borrow().next {
+                tmp = Rc::clone(node);
+                print!(" -> {}", tmp.borrow().value);
+            }
+            println!("");
+        } else {
+            println!("Empty");
+        }
+    }
+
+    fn merge(&self) -> LinkedList {
+        let mut list = LinkedList::new();
+        if let Some(root_node) = &self.root {
+            let mut slower = Rc::clone(root_node);
+            let mut faster = if let Some(node) = &root_node.borrow().next {
+                Rc::clone(node)
+            } else {
+                list.insert(root_node.borrow().value);
+                return list;
+            };
+
+            loop {
+                slower = Rc::clone(slower.clone().borrow().next.as_ref().unwrap());
+                if let Some(n1) = &faster.clone().borrow().next {
+                    if let Some(n2) = &n1.borrow().next {
+                        faster = Rc::clone(n2);
+                    } else {
+                        break;
+                    }
+                } else {
+                    break;
+                }
+            }
+
+            let mid = slower.clone();
+            let mut p1 = root_node.clone();
+            let mut p2 = mid.clone();
+
+            list.insert(p1.borrow().value);
+            list.insert(p2.borrow().value);
+            loop {
+                if let (Some(n1), Some(n2)) = (&p1.clone().borrow().next, &p2.clone().borrow().next)
+                {
+                    if Rc::ptr_eq(&n1, &mid) {
+                        break;
+                    }
+
+                    list.insert(n1.borrow().value);
+                    list.insert(p2.borrow().value);
+                    p1 = Rc::clone(n1);
+                    p2 = Rc::clone(n2);
+                } else {
+                    break;
+                }
+            }
+
+            if let Some(n2) = &p2.clone().borrow().next {
+                list.insert(n2.borrow().value);
+            }
+        }
+        return list;
+    }
 }
 
 fn main() {
     let mut list = LinkedList::new();
-    list.insert(9);
-    list.insert(2);
-    list.insert(6);
     list.insert(1);
-    list.insert(0);
     list.insert(3);
-    println!("{:?}", list);
-    list.remove(6);
-    println!("{:?}", list);
+    list.insert(5);
+    list.insert(7);
+    list.insert(9);
+    list.insert(11);
+    list.insert(2);
+    list.insert(4);
+    list.insert(6);
+    list.insert(8);
+    list.insert(10);
+    list.insert(12);
+    list.insert(14);
+    list.print();
+    let list2 = list.merge();
+    list2.print();
 }
